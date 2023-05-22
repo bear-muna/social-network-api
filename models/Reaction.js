e0c9a168b33e9c840087cfe48c99e4cd4ddb2672
bear-muna <bearmuna@gmail.com>
2023-05-22 13:57:32 -0700

    created reactionSchema and exported

---
+++ models/Reaction.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: new ObjectId,
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: [280, 'Too long of a reaction'],
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            // TODO: Create a getter to format date
+        }
+    }
+);
+
+module.exports = reactionSchema;
