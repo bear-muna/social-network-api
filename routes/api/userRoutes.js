74c62200b306ad26345c3c3baddcf77d5e0c7ad2
bear-muna <bearmuna@gmail.com>
2023-05-22 14:15:55 -0700

    created userRoutes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const {
+    getUsers,
+    getSingleUser,
+    createUser
+} = require('../../controllers/userController');
+
+// API GET USERS & CREATE USER
+router.route('/').get(getUsers).create(createUser);
+
+// API GET SINGLE USER
+router.route('/:id').get(getSingleUser);
+
+module.exports = router;
