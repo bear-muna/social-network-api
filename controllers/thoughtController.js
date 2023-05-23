68970c9bcdbc37069b457ce0041557ce640ac8b1
bear-muna <bearmuna@gmail.com>
2023-05-22 20:58:24 -0700

    created update and delete thought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -46,5 +46,47 @@ module.exports = {
             console.log(error);
             res.status(500).json({ msg: "Error creating a thought", error });
         }
+    },
+    // Update Thought by ID
+    async updateThought(req, res) {
+        try {
+            const updateThoughtData = await Thought.findOneAndUpdate(
+                { _id: req.params.id },
+                { $set: req.body },
+                { runValidators: true, new: true }
+            );
+            if (!updateThoughtData) {
+                return res.status(400).json({ msg: "No thought in db " });
+            }
+            res.json(updateThoughtData);
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ msg: "Error updating a thought", error });
+        }
+    },
+    // Delete Thought by ID
+    async deleteThought(req, res) {
+        try {
+            const delThought = await Thought.findOneAndDelete(
+                { _id: req.params.id },
+            );
+            if (!delThought) {
+                return res.status(400).json({ msg: "No thought exists in db" });
+            }
+            res.json(delThought)
+            // TODO: Extend this function to delete parent's subdocument of thoughts
+            // if (delThought) {
+            //     const dbUserData = await User.findOne({ thoughts: delThought._id });
+            //     if (dbUserData) {
+            //         const cascadeUser = await dbUserData.thoughts.pull( delThought._id );
+            //         res.json(cascadeUser);
+            //     } else {
+            //         return res.status(400).json({ msg: "No user exists in db" })
+            //     }
+            // }
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ msg: "Error deleting thought", error });
+        }
     }
 }
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -3,13 +3,15 @@ const router = require('express').Router();
 const {
     getThoughts,
     getSingleThought,
-    createThought
+    createThought,
+    updateThought,
+    deleteThought
 } = require('../../controllers/thoughtController');
 
 // API Get Thoughts & Create Thought
 router.route('/').get(getThoughts).post(createThought);
 
 // API Get Thoughts by ID
-router.route('/:id').get(getSingleThought);
+router.route('/:id').get(getSingleThought).put(updateThought).delete(deleteThought);
 
 module.exports = router;
